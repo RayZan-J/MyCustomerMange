cde0d9fe5d461b04cc7567b5033cade3717079a0
zll <qq466897636@163.com>
2017-09-18 23:17:09 +0800

    1.letcode--计算帕斯卡三角给定行数的值 2.用另一种方法实现帕斯卡三角（规律）

--- com.org/Action/LetCode/Generate.java
+++ com.org/Action/LetCode/Generate.java
@@ -8,8 +8,37 @@ import java.util.List;
 
 /**
  * Created by zll on 2017/9/14 0014.
+ * 帕斯卡三角
+ *    //[[1], [1, 1], [1, 2, 1], [1, 3, 3, 1],
+ *    [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1],
+ *    [1, 6, 15, 20, 15, 6, 1],
+ *    [1, 7, 21, 35, 35, 21, 7, 1]
+ *
+ *    数字有规律：如 [1, 7, 21, 35, 35, 21, 7, 1]：
+ *    7=1*7/1；
+ *    21 = 7*（7-1）/（1+1）
+ *    35 = 21*（7-2）/（2+1）
+ *    35 = 35*（7-3）/（3+1）
+ *    21 = 35*（7-4）/（4+1）
+ *    7 = 21*（7-5）/（5+1）；
+ *    1 = 7*（7-6）/（6+1）；
  */
 public class Generate {
+    public static List<List<Integer>> generateAnother(int numRows) {
+        List<Integer> list;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for(int i=0;i<numRows;i++){
+            int prev = 1;
+            list = new ArrayList<Integer>();
+            list.add(prev);
+            for(int j=0;j<i;j++){
+                prev = prev*(i-j)/(j+1);
+                list.add(prev);
+            }
+            result.add(list);
+        }
+        return result;
+    }
     public static List<List<Integer>> generate(int numRows) {
         List<Integer> list;
         List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -29,9 +58,10 @@ public class Generate {
 
     public static void main(String[] args) {
         System.out.println(generate(6).toString());
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(0,1);
-        list.add(0,2);
-        System.out.println(list.toString());
+        System.out.println(generateAnother(6).toString());
+//        List<Integer> list = new ArrayList<Integer>();
+//        list.add(0,1);
+//        list.add(0,2);
+//        System.out.println(list.toString());
     }
 }
---
+++ com.org/Action/LetCode/PascalTriangle.java
@@ -0,0 +1,31 @@
+package Action.LetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zll on 2017/9/18 0018.
+ * 求帕斯卡三角给定行数的行
+ */
+public class PascalTriangle {
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> temp;
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<rowIndex+1;i++){
+            temp = new ArrayList<>();
+            temp.add(0,1);
+            for(int j=1;j<i;j++){
+                temp.add(j,result.get(i-1).get(j-1)+result.get(i-1).get(j));
+            }
+            if(i>0){
+                temp.add(i,1);
+            }
+            result.add(temp);
+        }
+        return result.get(rowIndex);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRow(4).toString());
+    }
+}
