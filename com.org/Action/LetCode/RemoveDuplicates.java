de02949bbe2c567e5ce6a73ec7a0f9cab9de9edd
zll <qq466897636@163.com>
2017-09-26 22:50:10 +0800

    1.letcode--移除重复数据，返回新长度

--- com.org/Action/LetCode/RemoveDuplicates.java
+++ com.org/Action/LetCode/RemoveDuplicates.java
@@ -1,15 +1,37 @@
 package Action.LetCode;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by zll on 2017/9/18 0018.
+ * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+ Do not allocate extra space for another array, you must do this in place with constant memory.
+
+ For example,
+ Given input array nums = [1,1,2],
+
+ Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+ 给定一个排好序的数组，数组中有重复数据，求将重复数据去除后，剩余长度，即 1 1 2 2 3 去重后，得到1 2 3数组，返回3
  */
 public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums) {
-        int sums = 0;
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
 
-        return sums;
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
     }
 }
