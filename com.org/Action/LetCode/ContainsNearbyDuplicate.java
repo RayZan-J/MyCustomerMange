eb03f3d3cbdada2ba13c7270ecd2e422ca249422
zll <qq466897636@163.com>
2017-09-27 22:36:16 +0800

    1.letcode--求数组中给定范围内是否存在相同数据

---
+++ com.org/Action/LetCode/ContainsNearbyDuplicate.java
@@ -0,0 +1,15 @@
+package Action.LetCode;
+
+/**
+ * Created by zll on 2017/9/27 0027.
+ */
+public class ContainsNearbyDuplicate {
+    public static boolean containsNearbyDuplicate(int[] nums, int k){
+        for(int i=0;i<nums.length;i++){
+
+        }
+
+        return true;
+    }
+
+}
