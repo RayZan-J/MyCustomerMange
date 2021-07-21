581312b753eb86af0ee62ab0a6c576f3e5d6096a
zhangll <zhangll@bokecc.com>
2021-07-21 18:41:28 +0800

    数组中只有1个数不重复,找到这个数

---
+++ com.org/Action/LetCode/FindOnly.java
@@ -0,0 +1,39 @@
+package Action.LetCode;
+
+/**
+ * 题目:给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素
+ * <p>
+ * 思考:最笨的办法,用map,将次数存起来,然后遍历map,得到次数为1的key,但是不满足O(n)
+ *
+ * @author zll
+ * @since 2021-07-21 18:28
+ */
+public class FindOnly {
+    /**
+     * 原理:使用异或运算 ^
+     * <p></p>
+     * 异或运算有以下三个性质:
+     * <p></p>
+     * 1.任何数和 00 做异或运算，结果仍然是原来的数，即 a ^ 0=a,a ^ 0=a
+     * <p></p>
+     * 2.任何数和其自身做异或运算，结果是 00，即 a ^ a=0 ,a ^ a=0
+     * <p></p>
+     * 3.异或运算满足交换律和结合律，即 a ^ b ^ a=b ^ a ^ a=b ^ (a ^ a)=b ^ 0=b
+     * <p></p>
+     *
+     * @param nums
+     * @return
+     */
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 1, 2, 5, 3, 4};
+        System.out.println(singleNumber(nums));
+    }
+}
--- com.org/Action/LetCode/TwoSum2.java
+++ com.org/Action/LetCode/TwoSum2.java
@@ -7,53 +7,55 @@ import java.util.List;
 /**
  * Created by zll on 2017/9/26 0026.
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
- You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
- Example:
- Given nums = [2, 7, 11, 15], target = 9,
-
- Because nums[0] + nums[1] = 2 + 7 = 9,
- return [0, 1].
-
- 与之前的数组求和不同，这个数组没有排序，且有重复
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * <p></p>
+ * 与之前的数组求和不同，这个数组没有排序，且有重复
  */
 public class TwoSum2 {
     //我的方法，参考了排序数组，求和的方法
     public static int[] twoSum(int[] nums, int target) {
-        List<Integer> list = new ArrayList<Integer>();
-        for(int n:nums){
+        List<Integer> list = new ArrayList<>();
+        for (int n : nums) {
             list.add(n);
         }
         Arrays.sort(nums);
         int low = 0;
-        int max = nums.length-1;
-        while(low<=max){
-            int sum = nums[low]+nums[max];
-            if(sum==target){
-                return new int[]{list.indexOf(nums[low]),list.lastIndexOf(nums[max])};
-            }else if(sum<target){
+        int max = nums.length - 1;
+        while (low <= max) {
+            int sum = nums[low] + nums[max];
+            if (sum == target) {
+                return new int[]{list.indexOf(nums[low]), list.lastIndexOf(nums[max])};
+            } else if (sum < target) {
                 ++low;
-            }else{
+            } else {
                 --max;
             }
         }
-        return new int[]{0,0};
+        return new int[]{0, 0};
 
     }
+
     public static int[] twoSumOthers(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target) {
-                    return new int[]{i,j};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
             }
         }
         throw new IllegalArgumentException("no such two element find");
 
     }
+
     public static void main(String[] args) {
-        int[] nums = {3,2,4};
+        int[] nums = {3, 2, 4};
         int target = 6;
         System.out.println(Arrays.toString(twoSumOthers(nums, target)));
         System.out.println(Arrays.toString(twoSum(nums, target)));
